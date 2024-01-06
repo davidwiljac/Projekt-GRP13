58a5a9f6f291fdef0e1f1f4e1c006877135ac5e8
David <david.wilson.jacobsen@gmail.com>
2024-01-06 14:48:21 +0100

    added includes

--- Src/main.c
+++ Src/main.c
@@ -1,6 +1,14 @@
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" 		// Input/output library for this course
 
+#include "ansi.h"
+#include "bullet.h"
+#include "drivers.h"
+#include "enemy.h"
+#include "nuke.h"
+#include "powerup.h"
+#include "spaceship.h"
+
 int main(void)
 {
 	while(1){}
