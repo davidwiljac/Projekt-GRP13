0664eeff2c8e1efa8e84e778b4cf42a9f5e1d37e
Asger Lorenzen <lorenzen2410@gmail.com>
2024-01-06 15:17:35 +0100

    rettede fejl i types.h

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -6,24 +6,48 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Src/30010_io.c \
+../Src/ansi.c \
+../Src/bullet.c \
+../Src/drivers.c \
+../Src/enemy.c \
 ../Src/main.c \
+../Src/nuke.c \
+../Src/powerup.c \
+../Src/spaceship.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c \
-../Src/system_stm32f30x.c 
+../Src/system_stm32f30x.c \
+../Src/update.c 
 
 OBJS += \
 ./Src/30010_io.o \
+./Src/ansi.o \
+./Src/bullet.o \
+./Src/drivers.o \
+./Src/enemy.o \
 ./Src/main.o \
+./Src/nuke.o \
+./Src/powerup.o \
+./Src/spaceship.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o \
-./Src/system_stm32f30x.o 
+./Src/system_stm32f30x.o \
+./Src/update.o 
 
 C_DEPS += \
 ./Src/30010_io.d \
+./Src/ansi.d \
+./Src/bullet.d \
+./Src/drivers.d \
+./Src/enemy.d \
 ./Src/main.d \
+./Src/nuke.d \
+./Src/powerup.d \
+./Src/spaceship.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d \
-./Src/system_stm32f30x.d 
+./Src/system_stm32f30x.d \
+./Src/update.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -33,7 +57,7 @@ Src/%.o: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/30010_io.d ./Src/30010_io.o ./Src/main.d ./Src/main.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o ./Src/system_stm32f30x.d ./Src/system_stm32f30x.o
+	-$(RM) ./Src/30010_io.d ./Src/30010_io.o ./Src/ansi.d ./Src/ansi.o ./Src/bullet.d ./Src/bullet.o ./Src/drivers.d ./Src/drivers.o ./Src/enemy.d ./Src/enemy.o ./Src/main.d ./Src/main.o ./Src/nuke.d ./Src/nuke.o ./Src/powerup.d ./Src/powerup.o ./Src/spaceship.d ./Src/spaceship.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o ./Src/system_stm32f30x.d ./Src/system_stm32f30x.o ./Src/update.d ./Src/update.o
 
 .PHONY: clean-Src
 
--- Inc/types.h
+++ Inc/types.h
@@ -25,63 +25,7 @@ typedef struct {
 }vector_t;
 
 
-// gameState_t
-typedef struct {
-	bullet_node_t bulletLinkedList;
-	enemy_node_t enemyLinkedList;
-	moon_t moons[];
-	spaceship_t spaceship;
-	uint8_t cityLives;
-	uint16_t score;
-	uint8_t activeScreen;
-	uint8_t menuSelected;
-	uint8_t isDead;
-	uint8_t difficulty;
-} gameState_t ;
 
-
-//spaceShip_t
-typedef struct {
-	position_t position;
-	position_t nextPosition;
-	uint8_t numberOfParts;
-	uint8_t firingRate;
-	nuke_t nuke;
-	uint8_t powerups;
-} spaceship_t ;
-
-
-//enemy_t
-typedef struct {
-	position_t position;
-	position_t nextPosition;
-	vector_t velocity;
-	uint8_t firingRate;
-} enemy_t ;
-
-
-//nuke_t
-typedef struct {
-	position_t position;
-	position_t nextPosition;
-	uint8_t isDeployed;
-	uint8_t readyState; //(0=rød, 1=magenta, 2=gul, 3=grøn)
-	uint8_t timeSinceLastActivation; //(i sekunder)
-} nuke_t ;
-
-
-// bullet_node_t
-typedef struct {
-	bullet_t bullet;
-	bullet_t* nextBullet;
-}bullet_node_t;
-
-
-//enemy_node_t
-typedef struct {
-	enemy_t enemy;
-	enemy_t* nextEnemy;
-} enemy_node_t;
 
 
 //bullet_t
@@ -97,7 +41,46 @@ typedef struct {
 	position_t position;
 	position_t nextPosition;
 	vector_t velocity;
-}enemy_t;
+	uint8_t firingRate;
+} enemy_t ;
+
+// bullet_node_t
+typedef struct {
+	bullet_t bullet;
+	bullet_t* nextBullet;
+}bullet_node_t;
+
+
+//enemy_node_t
+typedef struct {
+	enemy_t enemy;
+	enemy_t* nextEnemy;
+} enemy_node_t;
+
+
+
+
+
+
+
+//nuke_t
+typedef struct {
+	position_t position;
+	position_t nextPosition;
+	uint8_t isDeployed;
+	uint8_t readyState; //(0=rød, 1=magenta, 2=gul, 3=grøn)
+	uint8_t timeSinceLastActivation; //(i sekunder)
+} nuke_t ;
+
+//spaceShip_t
+typedef struct {
+	position_t position;
+	position_t nextPosition;
+	uint8_t numberOfParts;
+	uint8_t firingRate;
+	nuke_t nuke;
+	uint8_t powerups;
+} spaceship_t ;
 
 
 //moon_t
@@ -107,7 +90,19 @@ typedef struct {
 }moon_t;
 
 
-
+// gameState_t
+typedef struct {
+	bullet_node_t bulletLinkedList;
+	enemy_node_t enemyLinkedList;
+	moon_t* moons;
+	spaceship_t spaceship;
+	uint8_t cityLives;
+	uint16_t score;
+	uint8_t activeScreen;
+	uint8_t menuSelected;
+	uint8_t isDead;
+	uint8_t difficulty;
+} gameState_t ;
 
 
 #endif /* TYPES_H_ */
