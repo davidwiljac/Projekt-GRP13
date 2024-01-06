9258ce6bda7a010e7c33600eb9fbf924e9e6358a
Asger Lorenzen <lorenzen2410@gmail.com>
2024-01-06 14:59:19 +0100

    En masse structs med datatyper

---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/30010_io.c \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32f30x.c 
+
+OBJS += \
+./Src/30010_io.o \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32f30x.o 
+
+C_DEPS += \
+./Src/30010_io.d \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32f30x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F302R8Tx -DSTM32F3 -DNUCLEO_F302R8 -DDEBUG -DUSE_STDPERIPH_DRIVER=1 -c -I"C:/Users/Loren/OneDrive - Danmarks Tekniske Universitet/DTU/1. semester/Programmeringsprojekt 30010/Projekt-GRP13/SPL/inc" -I"C:/Users/Loren/OneDrive - Danmarks Tekniske Universitet/DTU/1. semester/Programmeringsprojekt 30010/Projekt-GRP13/Inc" -I"C:/Users/Loren/OneDrive - Danmarks Tekniske Universitet/DTU/1. semester/Programmeringsprojekt 30010/Projekt-GRP13/cmsis" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/30010_io.d ./Src/30010_io.o ./Src/main.d ./Src/main.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o ./Src/system_stm32f30x.d ./Src/system_stm32f30x.o
+
+.PHONY: clean-Src
+
--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,7 @@
 #include "stm32f30x_conf.h" // STM32 config
 #include "30010_io.h" 		// Input/output library for this course
+#include "types.h"
+
 
 int main(void)
 {
