4bf9f44071764734a641d039da811b7b463dcce5
davidwiljac <david.wilson.jacobsen@gmail.com>
2024-01-09 11:43:25 +0100

    spawn og update enemy

--- Inc/graphics.h
+++ Inc/graphics.h
@@ -7,9 +7,13 @@
 
 #ifndef GRAPHICS_H_
 #define GRAPHICS_H_
+#include "ansi.h"
+#include "fixedPoint.h"
+#include "stdint.h"
+#include "stdio.h"
 
 void drawMoon(uint8_t x, uint8_t y);
-void drawEnemy();
+void drawEnemy(gameState_t* gameState);
 void drawBullet();
 void drawMe();
 void Moongravity();
